e4a503665a0f1cebe463e844da6b0b6a59a2e861
Nat Budin <natbudin@gmail.com>
2010-05-06 14:22:00 -0400

    Point to a local CAS server for example

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth. The first
 # four configuration values can also be set straight in your models.
 Devise.setup do |config|
+  config.cas_base_url = "http://localhost:8080/cas"
+
   # Configure the e-mail address which will be shown in DeviseMailer.
   config.mailer_sender = "please-change-me@config-initializers-devise.com"
   
